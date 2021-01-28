9e8a7521ac0e2b5d41014a03c8032774625634c3
Roberta Takenaka <roberta.takenaka@scielo.org>
2021-01-28 14:19:38 -0300

    Melhorias em `make_reports`

--- scripts/make_reports.py
+++ scripts/make_reports.py
@@ -4,14 +4,28 @@ import shutil
 import json
 import logging
 from datetime import datetime
+import time
 
-
-NOT_CONVERTED = []
-NOT_PACKED = []
-NOT_IMPORTED = []
-NOT_LINKED = []
 MISSING_FILES = {}
 OTHERS = []
+
+STATUS = {}
+STATUS['not_converted'] = {'label': 'Não convertidos', 'items': []}
+STATUS['not_packed'] = {'label': 'Não empacotados', 'items': []}
+STATUS['pack_incomplete'] = {'label': 'Incompleto', 'items': []}
+STATUS['not_imported'] = {'label': 'Não importados', 'items': []}
+STATUS['not_linked'] = {'label': 'Não linkados', 'items': []}
+
+
+def logfileinfo(file_path):
+    try:
+        created_at = "[%s, criado em: %s]" % (file_path, time.ctime(os.path.getctime(file_path)))
+        with open(file_path, "r") as fp:
+            content = fp.read().splitlines()
+    except FileNotFoundError:
+        created_at = "unknown creation date"
+        content = [f"Arquivo {file_path} não encontrado"]
+    return created_at, content
 
 
 def trace(year):
@@ -24,24 +38,45 @@ def trace(year):
     packaged_path = os.path.join(year_path, "packaged")
     link2kernel_path = os.path.join(year_path, "link2kernel")
 
+    files_info = []
+    for dirname in (link2kernel_path, logs_path):
+        for filename in os.listdir(dirname):
+            file_path = os.path.join(dirname, filename)
+            stat = os.stat(file_path)
+            files_info.append(
+                (
+                    datetime.fromtimestamp(stat.st_mtime).isoformat(),
+                    file_path
+                )
+            )
+    files_info = sorted(files_info)
+
     pids_to_extract = get_pids(pids_file)
     pids_in_conversion = get_pids_in_conversion(conversion_path)
     pids_in_packaged = get_pids_in_packaged(packaged_path)
 
-    result = []
+    result = ["", str(year), "----"]
+
+    result += ['']
+    result += [
+        "%s\t%s" % (d, filename)
+        for d, filename in files_info
+    ]
+    result += ['']
     result += inform_1(
                 len(pids_to_extract), len(pids_in_conversion),
                 len(os.listdir(conversion_path)), len(pids_in_packaged))
 
-    with open(f"{reports_path}/report.txt", "r") as fp:
-        result += [
-            fp.read()
-        ]
+    created_at, rows = logfileinfo(f"{reports_path}/report.txt")
+    result += rows
 
-    imported = read_import_json(link2kernel_path)
+    # pack
     pack_log = os.path.join(logs_path, "pack.log")
     missing_img, missing_pdf, other, _result = packing_errors(pack_log)
     result += _result
+
+    # import
+    created_at, imported = read_import_json(link2kernel_path)
 
     errors_grouped_by_pid_v2 = group_packing_errors_by_pid_v2(
         missing_img, missing_pdf, other)
@@ -56,25 +91,21 @@ def trace(year):
         if data["pid_v3"] in pid_v3_not_linked:
             not_linked.append(pid_v2)
 
-    not_converted = pids_to_extract - pids_in_conversion
-    result += register_status("Não convertidos", not_converted, NOT_CONVERTED)
+    not_converted = sorted(pids_to_extract - pids_in_conversion)
+    not_packed = sorted(pids_to_extract - pids_in_packaged)
 
-    not_packaged = pids_to_extract - pids_in_packaged
-    result += register_status("Não empacotados", not_packaged, NOT_PACKED)
+    not_imported = sorted(pids_to_extract - set(imported.keys()))
 
-    not_imported = pids_to_extract - set(imported.keys())
-    result += register_status("Não importados", not_imported, NOT_IMPORTED)
-
-    result += register_status("Não linkados", not_linked, NOT_LINKED)
+    values = (
+        not_converted, not_packed, sorted(errors_grouped_by_pid_v2.keys()),
+        not_imported, sorted(not_linked))
+    for key, items in zip(STATUS.keys(), values):
+        STATUS[key]['items'] += items
+        result += lista_pid(STATUS[key]['label'], items)
 
     OTHERS.extend(other)
 
     return result
-
-
-def register_status(status_label, items, global_var):
-    global_var.extend(list(items))
-    return lista_pid(status_label, items)
 
 
 def read_import_json(link2kernel_path):
@@ -84,24 +115,24 @@ def read_import_json(link2kernel_path):
      "year": "1999", "volume": "56", "number": "1", "order": "00009"}
     """
     imported = {}
-
+    _created_at = []
     for filename in sorted(os.listdir(link2kernel_path)):
         name, ext = os.path.splitext(filename)
         if ext == ".json" and name.startswith("imported."):
             file_path = os.path.join(link2kernel_path, filename)
-            with open(file_path, "r") as fp:
-                items = fp.read().splitlines()
-            for item in items:
+            
+            created_at, rows = logfileinfo(file_path)
+            _created_at += created_at
+            for item in rows:
                 data = json.loads(item)
                 imported[data["pid"]] = data
-    return imported
+    return _created_at, imported
 
 
 def lista_pid(label, items):
     rows = [
         "\n____",
-        label,
-        str(len(items)),
+        "%s: %i" % (label, len(items)),
     ]
     rows.extend(sorted(items))
     return rows
@@ -111,8 +142,7 @@ def display_errors(label, items):
     return [
         "\n",
         "----####----",
-        label,
-        str(len(items)),
+        "%s: %i" % (label, len(items)),
         "\n".join(items),
         "----####----",
         "",
@@ -161,16 +191,13 @@ def get_pids_in_packaged(packaged_path):
 
 
 def get_errors_in_logfile(log_file_path):
-    try:
-        with open(log_file_path, "r") as fp:
-            lines = fp.read().splitlines()
-        return [
-            line
-            for line in lines
-            if "ERROR" in line
-        ]
-    except IOError as e:
-        logging.exception(e)
+    with open(log_file_path, "r") as fp:
+        lines = fp.read().splitlines()
+    return [
+        line
+        for line in lines
+        if "ERROR" in line
+    ]
 
 
 def packing_errors(pack_log):
@@ -180,8 +207,10 @@ def packing_errors(pack_log):
      'new_fname': '1807-0337-soc-14-376-437-ga14qdr01',
      'msg': 'Not found /img/revistas/soc/n14/a14qdr01.gif'}
     """
-    errors = get_errors_in_logfile(pack_log)
-
+    try:
+        errors = get_errors_in_logfile(pack_log)
+    except FileNotFoundError:
+        errors = [f"Arquivo não encontrado {pack_log}"]
     missing_img = [
         msg
         for msg in errors
@@ -206,8 +235,7 @@ def packing_errors(pack_log):
             ("missing img", "missing pdf", "others"),
             (missing_img, missing_pdf, other)):
         result += [
-            error_type,
-            str(len(msg)),
+            "%s: %i" % (error_type, len(msg))
         ]
 
     return missing_img, missing_pdf, other, result
@@ -232,22 +260,27 @@ def group_packing_errors_by_pid_v2(missing_img, missing_pdf, other):
                 try:
                     msg = msg.replace("'", '"')
                     data = json.loads(msg)
-                except json.decoder.JSONDecodeError:
-                    logging.exception(msg)
+                except json.decoder.JSONDecodeError as e:
+                    logging.exception(
+                        "Unable to execute `json.loads()` with `%s`. %s" % (msg, e))
 
                 else:
                     pid_v2 = data["pid"]
-                    data["date"] = splitted[0]
-                    failures[pid_v2] = failures.get(pid_v2) or []
-                    failures[pid_v2].append(data)
+                    failures[pid_v2] = failures.get(pid_v2) or {
+                        "id": pid_v2, "items": []}
+                    new_data = {"date": splitted[0]}
+                    new_data.update(data)
+                    failures[pid_v2]["items"].append(
+                        new_data
+                    )
     return failures
 
 
 def linking_errors(link_log):
-    errors = get_errors_in_logfile(link_log)
-
-    if not errors:
-        return [], []
+    try:
+        errors = get_errors_in_logfile(link_log)
+    except FileNotFoundError:
+        errors = [f"Arquivo não encontrado {link_log}"]
 
     result = display_errors("link errors", errors)
 
@@ -276,19 +309,19 @@ def main():
     y_begin, y_end = args.begin, args.end
     if y_begin > y_end:
         y_begin, y_end = y_end, y_begin
-    y_end += 1
 
     report_path = os.path.join(
-        args.report_path, datetime.now().isoformat().replace(":", ""))
+        args.report_path, datetime.now().isoformat()[:10])
     if not os.path.isdir(report_path):
         os.makedirs(report_path)
 
     result = []
-    for year in sorted(range(y_begin, y_end), reverse=True):
+    for year in sorted(range(y_begin, y_end+1), reverse=True):
         print(year)
         result += trace(year)
 
-    save_items(os.path.join(report_path, f"{y_begin}-{y_end}.txt"), result)
+    y_range = f"{y_begin}-{y_end}"
+    save_items(os.path.join(report_path, f"{y_range}.txt"), result)
 
     if args.badfiles:
         with open("pids_html/0000_html.txt", "w") as fp:
@@ -301,16 +334,18 @@ def main():
                 dst = f"html_migracao/0000/source/{pid}.{ext}"
                 shutil.copy(src, dst)
 
-    labels = ("not_converted", "not_packed", "not_imported", "not_linked")
-    lists = (NOT_CONVERTED, NOT_PACKED, NOT_IMPORTED, NOT_LINKED)
-    for label, items in zip(labels, lists):
-        file_path = os.path.join(report_path, f"{label}.txt")
-        save_items(file_path, sorted(items))
+    for i, item in enumerate(STATUS.items()):
+        key, data = item
+        file_path = os.path.join(report_path, f"{y_range}-{i}-{key}.txt")
+        save_items(file_path, sorted(data["items"]))
 
-    save_items(os.path.join(report_path, "others.txt"), OTHERS)
+    save_items(os.path.join(report_path, f"{y_range}-other_errors.txt"), OTHERS)
 
-    with open(os.path.join(report_path, "missing_files.json"), "w") as fp:
-        fp.write(json.dumps(MISSING_FILES))
+    with open(os.path.join(report_path, f"{y_range}-missing_files.json"), "w") as fp:
+        fp.write(json.dumps(list(MISSING_FILES.values())))
+
+    print(report_path)
+    os.system(f"ls -ltr {report_path}")
 
 
 if __name__ == "__main__":
